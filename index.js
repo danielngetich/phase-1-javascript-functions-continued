4d6392c1048cd02c402f51662347cc54e72ae725
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-08-17 18:31:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(name="roller-skate"){
+    return (`This Saturday, I want to ${name}!`);
+}
+
+function mondayWork(note='go to the office'){
+    return (`This Monday, I will ${note}.`);
+}
+
+// function wrapAdjective(res="You are"){
+//     let result = wrapAdjective('*')
+//     let emphatic = result("a hard worker")
+//     return `${res} ${emphatic} `
+// }
+// wrapAdjective("%")("a dedicated programmer");
+
+function wrapAdjective(greet="*") {
+    return function (name) {
+      return `You are ${greet}${name}${greet}!`;
+    };
+  }
+  
